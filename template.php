a048600059224b073a3eaefe4970166e3c94ac12
Asheesh Laroia <asheesh@asheesh.org>
2011-08-03 23:40:21 +0200

    Ditch the '<?xml' thing

--- template.php
+++ template.php
@@ -1,8 +1,7 @@
 <?php
 $site_title = "Open Source Immersion at UPenn";
 $BASE="http://penn.openhatch.org/";
-?><?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 <title><?php echo $page_title?> (within <?php echo $site_title ?>)</title>
