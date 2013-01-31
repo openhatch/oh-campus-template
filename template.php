b7007cb5ac76ad17cbfbd998ebf817c60ad9fa37
Shauna GM <shaunagm@gmail.com>
2013-01-31 16:19:42 -0500

    changed template name

--- template.php
+++ template.php
@@ -1,5 +1,5 @@
 <?php
-$site_title = "Open Source Comes to Campus: Harvard University";
+$site_title = "Open Source Comes to Campus at Harvard";
 $BASE="http://harvard.openhatch.org/";
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
